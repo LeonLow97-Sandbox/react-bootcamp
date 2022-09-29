52e43f8e10cdf736091aaeec85f0933b160b7e5a
Leon Low <leonlow@email.com>
2022-09-29 10:08:30 +0800

    react basics & working with components

---
+++ 02-react-basics-and-components/react-complete-guide/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));  // rendered in index.html with <div id="root"></div>
+root.render(<App />);
