9965d7f514c1c5d89327cad84df8241535379fe3
Leon Low <lowjiewei@gmail.com>
2022-11-27 21:34:48 +0800

    React Custom Hooks

---
+++ 13-custom-hook-example/src/components/NewTask/NewTask.js
@@ -0,0 +1,41 @@
+// "https://react-http-a0421-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: "https://react-http-a0421-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      // bind method allows you to "preconfigure" which arguments that function should
+      // receive when it's eventually getting called.
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
