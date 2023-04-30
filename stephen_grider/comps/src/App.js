8e0ec360560ff8c47c4fb2b06395cbbe03a5c74e
LeonLow97 <lowjiewei@gmail.com>
2023-04-30 16:17:44 +0800

    Added styling to dropdown

--- stephen_grider/comps/src/App.js
+++ stephen_grider/comps/src/App.js
@@ -14,7 +14,12 @@ function App() {
     { label: "Blue", value: "blue" },
   ];
 
-  return <Dropdown options={options} selection={selection} onSelect={handleSelect} />;
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
 }
 
 export default App;
