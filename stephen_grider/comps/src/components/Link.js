e623a8d350d791291130cf06ddca16778a4eccdc
LeonLow97 <lowjiewei@gmail.com>
2023-05-01 13:35:57 +0800

    metaKey and ctrlKey

--- stephen_grider/comps/src/components/Link.js
+++ stephen_grider/comps/src/components/Link.js
@@ -5,12 +5,22 @@ function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    // If user holds control key and click link, default browser behavior is to open a new tab.
+    // check for metaKey (macOS) and ctrlKey (windows). If true, we follow the default browser behavior
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
 
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
