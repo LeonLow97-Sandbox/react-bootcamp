27e88fcdb280caa94d68a183dc0fee00dba88605
LeonLow97 <lowjiewei@gmail.com>
2023-05-12 00:00:50 +0800

    Creating reusable Table component

--- stephen_grider/comps/src/App.js
+++ stephen_grider/comps/src/App.js
@@ -1,9 +1,10 @@
-import Sidebar from "./components/Sidebar";
-import Route from "./components/Route";
-import AccordionPage from "./pages/AccordionPage";
-import DropdownPage from "./pages/DropdownPage";
-import ButtonPage from "./pages/ButtonPage";
-import ModalPage from "./pages/ModalPage";
+import Sidebar from './components/Sidebar';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
+import ModalPage from './pages/ModalPage';
+import TablePage from './pages/TablePage';
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
         </Route>
         <Route path="/modal">
           <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
         </Route>
       </div>
     </div>
