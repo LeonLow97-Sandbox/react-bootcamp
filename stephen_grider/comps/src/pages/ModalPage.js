0a81d0357fd9702b2bcca5c7d1d32c6ca9104d8c
LeonLow97 <lowjiewei@gmail.com>
2023-05-11 23:52:02 +0800

    Added CSS and button logic to modal

--- stephen_grider/comps/src/pages/ModalPage.js
+++ stephen_grider/comps/src/pages/ModalPage.js
@@ -9,16 +9,150 @@ function ModalPage() {
     setShowModal(true);
   };
 
+  // To close the modal by clicking anywhere outside the modal or button in the modal
   const handleClose = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
 
   return (
     <div className="relative">
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal onClose={handleClose} />}
+      {showModal && modal}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel leo
+        nec odio consectetur luctus. Phasellus ut nibh sagittis, tincidunt elit
+        in, convallis urna. Donec justo augue, gravida ultricies porttitor sed,
+        interdum vitae leo. Vestibulum blandit ipsum quis elementum porttitor.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris id massa a arcu ultrices suscipit a ac eros. Donec
+        id mauris volutpat, feugiat lacus eget, vulputate nibh. Curabitur
+        sollicitudin ac lectus sed ullamcorper. Quisque nec congue ligula. Duis
+        nulla justo, vehicula a eros ac, fermentum ornare augue. Aenean blandit
+        sem in laoreet venenatis. Integer efficitur quis mauris non tincidunt.
+      </p>
     </div>
   );
 }
