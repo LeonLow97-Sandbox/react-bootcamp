20771fa4d62a59641ae2e297cee4fb842c944269
LeonLow97 <lowjiewei@gmail.com>
2023-05-19 01:04:08 +0800

    Loading and error state to React components

--- stephen_grider/media/src/components/UsersList.js
+++ stephen_grider/media/src/components/UsersList.js
@@ -1,28 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
-import Button from './Button'
+import Button from './Button';
 import Skeleton from './Skeleton';
 
 function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
   const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users; // { data: [], isLoading: false, error: null}
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((err) => setLoadingUsersError(err))
+      .finally(() => setIsLoadingUsers(false));
   }, [dispatch]);
 
   const handleUserAdd = () => {
-    dispatch(addUser())
-  }
+    dispatch(addUser());
+  };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error Fetching Data...</div>;
   }
 
@@ -36,13 +43,15 @@ function UsersList() {
     );
   });
 
-  return <div>
-    <div className="flex flex-row justify-between m-3" >
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={handleUserAdd} >+ Add User</Button>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
     </div>
-    {renderedUsers}
-    </div>;
+  );
 }
 
 export default UsersList;
